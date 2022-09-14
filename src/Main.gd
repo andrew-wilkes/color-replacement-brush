3b182317777dc35870060fe85518bdc238ddb7a0
andrew-wilkes <github@urgentclick.com>
2022-09-14 11:51:30 +0900

    Add file IO logic

--- src/Main.gd
+++ src/Main.gd
@@ -1,5 +1,63 @@
 extends HBoxContainer
 
+enum { SAVING, LOADING }
+
+onready var file_dialog: FileDialog = $c/FileDialog
+
+var settings
+var file_mode
+
+func _init():
+	settings = Settings.new()
+	settings = settings.load_data()
+
 
 func _ready():
 	pass
+
+
+func _on_LoadImage_pressed():
+	file_mode = LOADING
+	file_dialog.mode = FileDialog.MODE_OPEN_FILE
+	file_dialog.current_dir = settings.load_dir
+	file_dialog.current_path = ""
+	file_dialog.current_file = ""
+	file_dialog.popup_centered()
+
+
+func _on_SaveImage_pressed():
+	file_mode = SAVING
+	file_dialog.mode = FileDialog.MODE_SAVE_FILE
+	file_dialog.current_dir = settings.save_dir
+	file_dialog.current_path = ""
+	file_dialog.current_file = ""
+	file_dialog.popup_centered()
+
+
+func _on_FileDialog_file_selected(path):
+	match file_mode:
+		LOADING:
+			settings.load_dir = path.get_base_dir()
+		SAVING:
+			settings.save_dir = path.get_base_dir()
+	print(path)
+
+
+func _unhandled_input(event):
+	if event is InputEventKey:
+		if event.pressed and event.scancode == KEY_ESCAPE:
+			save_and_quit()
+
+
+# Handle shutdown of App
+func _notification(what):
+	if what == MainLoop.NOTIFICATION_WM_QUIT_REQUEST:
+		save_and_quit()
+
+
+func save_and_quit():
+	settings.save_data()
+	get_tree().quit()
+
+
+
---
+++ src/settings.gd
@@ -0,0 +1,24 @@
+extends Resource
+
+class_name Settings
+
+const FILENAME = "user://settings.res"
+
+export var load_dir = ""
+export var save_dir = ""
+export var tolerance = 0.0
+export var brush_size = 16
+export var target_color = Color.white
+export var replacement_color = Color.green
+
+func save_data():
+	var _e = ResourceSaver.save(FILENAME, self)
+
+
+func load_data():
+	if ResourceLoader.exists(FILENAME):
+		return ResourceLoader.load(FILENAME)
+	else:
+		load_dir = OS.get_system_dir(OS.SYSTEM_DIR_DOCUMENTS)
+		save_dir = load_dir
+		return self
